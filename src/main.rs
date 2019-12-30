6f8755c7442254c2160561e06d714b4479253338
Robert Ou <rqou@robertou.com>
2019-12-29 16:19:36 -0800

    Add RTFM

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,13 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
-use atsamd11;
+use cortex_m_semihosting::hprintln;
+use rtfm::app;
 
-#[entry]
-fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
-    loop {
-        // your code goes here
+#[app(device = atsamd11)]
+const APP: () = {
+    #[init]
+    fn init(_: init::Context) {
+        hprintln!("Hello world!").unwrap();
     }
-}
+};
