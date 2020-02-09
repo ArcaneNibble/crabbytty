75dd451a95d2a61b804643e75eef1b10b28ef2d2
R. Ou <rqou@robertou.com>
2020-02-08 22:55:04 -0800

    new shift function for poke tool

--- poke_device.py
+++ poke_device.py
@@ -20,6 +20,8 @@ def jtag_bit(dev, tms, tdi):
     print("tms {} tdi {} tdo {}".format(tms, tdi, tdo))
     return tdo
 
+_last_bit = 0
+
 def go_tlr(dev):
     print("go tlr")
     jtag_bit(dev, 1, 0)
@@ -28,28 +30,69 @@ def go_tlr(dev):
     jtag_bit(dev, 1, 0)
     jtag_bit(dev, 1, 0)
 
-def shift_ir_from_tlr(dev):
-    print("shift ir")
-    jtag_bit(dev, 0, 0)
-    jtag_bit(dev, 1, 0)
-    jtag_bit(dev, 1, 0)
-    jtag_bit(dev, 0, 0)
+def rti_from_tlr(dev):
+    print("tlr -> rti")
     jtag_bit(dev, 0, 0)
 
-def shift_dr_from_tlr(dev):
+def shift_dr_from_rti(dev):
+    global _last_bit
     print("shift dr")
-    jtag_bit(dev, 0, 0)
     jtag_bit(dev, 1, 0)
     jtag_bit(dev, 0, 0)
-    bit0 = jtag_bit(dev, 0, 0)
-    return bit0
+    _last_bit = jtag_bit(dev, 0, 0)
+
+def shift_bits(dev, bits_in):
+    global _last_bit
+    bits_out = []
+    print("shifting {} bits".format(len(bits_in)))
+    for i in range(len(bits_in)):
+        bits_out.append(_last_bit)
+        _last_bit = jtag_bit(dev, 0, bits_in[i])
+    return bits_out
+
+def arr2num(arr):
+    ret = 0
+    for i in range(len(arr)):
+        ret |= arr[i] << i
+    return ret
+
+def num2arr(num, bits):
+    ret = []
+    for i in range(bits):
+        bit = num & (1 << i)
+        ret.append(1 if bit else 0)
+    return ret
+
+# def shift_ir_from_tlr(dev):
+#     print("shift ir")
+#     jtag_bit(dev, 0, 0)
+#     jtag_bit(dev, 1, 0)
+#     jtag_bit(dev, 1, 0)
+#     jtag_bit(dev, 0, 0)
+#     jtag_bit(dev, 0, 0)
+
+# def shift_dr_from_tlr(dev):
+#     print("shift dr")
+#     jtag_bit(dev, 0, 0)
+#     jtag_bit(dev, 1, 0)
+#     jtag_bit(dev, 0, 0)
+#     bit0 = jtag_bit(dev, 0, 0)
+#     return bit0
 
 go_tlr(dev)
+rti_from_tlr(dev)
+shift_dr_from_rti(dev)
 print("idcode")
-idcode = shift_dr_from_tlr(dev)
-for i in range(31):
-    idcode |= (jtag_bit(dev, 0, 0) << (i + 1))
-print("idcode is 0x{:08X}".format(idcode))
+idcode = shift_bits(dev, num2arr(0x12345678, 32))
+# idcode = shift_dr_from_tlr(dev)
+# for i in range(31):
+#     idcode |= (jtag_bit(dev, 0, 0) << (i + 1))
+print("idcode is 0x{:08X}".format(arr2num(idcode)))
+
+test = shift_bits(dev, num2arr(0xA53CFACE, 32))
+print("test is 0x{:08X}".format(arr2num(test)))
+test = shift_bits(dev, [0, 0] * 16)
+print("test is 0x{:08X}".format(arr2num(test)))
 # shift_ir_from_tlr(dev)
 
 # print("sniff irlen")
